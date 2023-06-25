e97d0a1811e38db1ba8ae1d4e3d75a9910145468
shreya raj <shreyaraj25032002@gmail.com>
2023-06-25 23:29:05 +0530

    user state redux added

---
+++ src/Redux/store.js
@@ -0,0 +1,42 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./userslice.js";
+
+//persister is used so that after refreshing the page user remain signed in.
+//it stores user and other elements in the local storage.
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/es/integration/react";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
-// import { useNavigate } from "react-router-dom";
-import './Login.css'
+import {useDispatch} from "react-redux";
+import { loginFailure, loginSuccess, loginStart } from "../../Redux/userslice.js";
+import { useNavigate } from "react-router-dom";
+import './Login.css';
+import axios from "axios";
 
 const LoginForm = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const dispatch = useDispatch();
+  const handleLogin = async(e)=>{
+    //it prevent refreshing of page on clicking signin button.
+    e.preventDefault();
+    dispatch(loginStart());
+    try{
+      const res = await axios.post("/login", { username, password });
+      dispatch(loginSuccess(res.data));
+      navigate(`/`);
+    }
+    catch(err){
+      dispatch(loginFailure(err));
+    }
+  }
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   };
@@ -15,16 +31,16 @@ const LoginForm = () => {
     setPassword(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Login submitted');
-    console.log('Username:', username);
-    console.log('Password:', password);
-    setUsername('');
-    setPassword('');
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log('Login submitted');
+  //   console.log('Username:', username);
+  //   console.log('Password:', password);
+  //   setUsername('');
+  //   setPassword('');
+  // };
 
-  // Print username and password on console
+  //Print username and password on console
   console.log('Username:', username);
   console.log('Password:', password);
 
@@ -38,7 +54,7 @@ const LoginForm = () => {
         <div className='right'>
           <div className='form-container'>
             <h2 className='form-title'>Welcome Back,</h2>
-            <form className='login-form' onSubmit={handleSubmit}>
+            <form className='login-form' onSubmit={handleLogin}>
               <label htmlFor='username'>Username:</label>
               <input
                 type='text'
